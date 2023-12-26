b7e202cca456269c6637905dedead0117bd377b2
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-26 12:36:33 +0530

    used deployed backend

--- src/context/HostContextProvider.jsx
+++ src/context/HostContextProvider.jsx
@@ -3,7 +3,7 @@ import HostContext from './HostContext'
 
 const HostContextProvider = ({children}) => {
 
-    const [host,setHost] = useState('http://localhost:3001');
+    const [host,setHost] = useState('https://rideshare-backend-q0sr.onrender.com');
     
   return (
     <HostContext.Provider value={{host,setHost}}>
